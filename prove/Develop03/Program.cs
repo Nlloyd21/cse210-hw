d290ecda681e3346840b945d2a84c456347fe48b
Nlloyd21 <llo21008@byui.edu>
2024-06-03 09:42:12 -0600

    scripture program

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,9 +1,30 @@
 using System;
 
 class Program
-{
-    static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        
+        static void Main()
+        {
+            Scripture scripture = new Scripture("Mosiah", 2, 17, "And behold, I tell you these things that ye may learn wisdom; that ye may learn that when ye are in the service of your fellow beings ye are only in the service of your God. ");
+            Console.Clear();
+            scripture.Display(); 
+            Console.WriteLine("\nPress Enter to continue or type 'quit' to exit.");
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input.ToLower() == "quit")
+                    break;
+
+                if (!scripture.HideRandomWord())
+                {
+                    Console.WriteLine("\n Congratulations! You memorized the scripture!");
+                    break;
+                }
+
+                Console.Clear();
+                scripture.Display();
+                Console.WriteLine("\nPress Enter to continue or type 'quit' to exit.");
+            }
+        }
     }
-}
